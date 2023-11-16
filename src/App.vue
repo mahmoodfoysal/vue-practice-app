3b10ef4a34f9d2e840ccdaa9601cde3b891b3fd0
mahmoodfoysal <foysalcse033@gmail.com>
2023-11-16 16:51:36 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import Home from './components/Home/Home/Home.vue';
+import SideBar from './components/SideBar/SideBar.vue';
+import NavBar from './components/NavBar/NavBar.vue';
+
+</script>
+
+<template>
+  <NavBar></NavBar>
+  <div class="grid grid-cols-12 gap-4">
+    <div class="col-span-3">
+      <SideBar></SideBar>
+    </div>
+    <div class="col-span-9">
+      <Home></Home>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/Home/Home/Home.vue
@@ -0,0 +1,10 @@
+<style scoped>
+
+</style>
+<template>
+    <h1 class="text-6xl">This is home component</h1>
+</template>
+
+<script setup>
+
+</script>
