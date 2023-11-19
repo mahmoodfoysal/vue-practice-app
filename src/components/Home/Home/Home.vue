5e6e3b94152335231b747dabcaf6898c975068e6
mahmoodfoysal <foysalcse033@gmail.com>
2023-11-20 03:17:54 +0600

    accoridion added

--- src/components/Home/Home/Home.vue
+++ src/components/Home/Home/Home.vue
@@ -1,23 +1,92 @@
 <style scoped>
 .main-container {
-    height: 100%;
-    background: #FFFFFF;
     box-shadow: 0px 0px 8px rgba(2, 108, 209, 0.1);
-    border-radius: 8px;
-    overflow: hidden;
-    padding: 10px 0;
+}
+.dollar-icon {
+    text-align: center;
+    font-size: 30px;
+    border-radius: 50%;
+    border: none;
+    background: gray;
+    color: white;
+    vertical-align: middle;
+}
+.user-icon {
+    text-align: center;
+    font-size: 30px;
+    border-radius: 50%;
+    /* border: 1px solid gray; */
+    background: white;
+    color: gray;
+    vertical-align: middle;
+}
+.stock-icon {
+    text-align: center;
+    font-size: 30px;
+    border-radius: 50%;
+    /* border: 1px solid gray; */
+    background: white;
+    color: gray;
+    vertical-align: middle;
+}
+.ticket-icon {
+    text-align: center;
+    font-size: 30px;
+    background: white;
+    color: gray;
+    vertical-align: middle;
 }
 </style>
 <template>
-
-    <section class="main-container">
-        <div class="">
-            <h4 class="text-2xl">Deshboard</h4>
-            <button class="text-xl text-white bg-teal-500 rounded p-2">Go To Main Website</button>
+    <section class="main-container bg-[#f3f3f3] rounded-lg py-2 w-[98%] h-[100%] overflow-hidden">
+        <div class="flex justify-between border-b-2 border-#e2edfa pb-3">
+            <h4 class="text-2xl decoration-gray-900 mx-5">Deshboard</h4>
+            <button class="text-white bg-violet-700 rounded p-2 mx-5">Go To Website</button>
         </div>
-        <h1 class="text-6xl">This is home component</h1>
+
+        <!-- value, user, ticket  -->
+        <section class="flex gap-x-5 px-5 mt-5">
+            <div class="flex items-center justify-between w-1/4 rounded-lg px-4 box-border bg-[#FFFFFF] z-10 h-16">
+                <div>
+                    <p class="text-xs">Value</p>
+                    <h6 class="text-sm">$30,000</h6>
+                </div>
+                <span class="material-icons dollar-icon">
+                    monetization_on
+                </span>
+            </div>
+            <div class="flex items-center justify-between w-1/4 rounded-lg px-4 box-border bg-[#FFFFFF] z-10">
+                <div>
+                    <p class="text-xs">Users</p>
+                    <h6 class="text-sm">50,021</h6>
+                </div>
+                <span class="material-icons user-icon">
+                    groups
+                </span>
+            </div>
+            <div class="flex items-center justify-between w-1/4 rounded-lg px-4 box-border bg-[#FFFFFF] z-10">
+                <div>
+                    <p class="text-xs">Stock</p>
+                    <h6 class="text-sm">45021</h6>
+                </div>
+                <span class="material-icons stock-icon">
+                    trending_up
+                </span>
+            </div>
+            <div class="flex items-center justify-between w-1/4 rounded-lg px-4 box-border bg-[#FFFFFF] z-10">
+                <div>
+                    <p class="text-xs">Ticket</p>
+                    <h6 class="text-sm">20516</h6>
+                </div>
+                <span class="material-icons ticket-icon">
+                    confirmation_number
+                </span>
+            </div>
+        </section>
+
     </section>
 </template>
+
 
 <script setup>
 
--- src/components/SideBar/SideBar.vue
+++ src/components/SideBar/SideBar.vue
@@ -1,52 +1,97 @@
 <style scoped>
-        .side-bar-bg {
-        background-color: aliceblue;
-        height: 562px;
-    }
-    .layout-sidebar {
-        height: 562px;
-        background-color: transparent;
-    }
 
-    .deviation-list {
-    /* min-width: var(--sidebar-width); */
-    width: 100%;
-    border-right: 1px solid #e2edfa;
+.layout-sidebar {
+    height: 562px;
+    background-color: transparent;
 }
 
+.deviation-list {
+    /* min-width: var(--sidebar-width); */
+    border-right: 1px solid #e2edfa;
+}
 </style>
 <template>
-    <section class="layout-sidebar deviation-list">
-        <div class="ps-5 pt-5">
-        <ul>
-            <li>Fruits & Vegetables
-                <ul class="ms-5">
-                    <li>Fresh Vegetables</li>
-                    <li>Fresh Fruits</li>
-                </ul>
-            </li>
-            <li>Meat & Fish
-                <ul class="ms-5">
-                    <li>Chicken & Poultry</li>
-                    <li>Premium Perishables</li>
-                    <li>Frozen Fish</li>
-                    <li>Meat</li>
-                    <li>Dried Fish</li>
-                </ul>
-            </li>
-            <li>Cooking
-                <ul class="ms-5">
-                    <li>Spices</li>
-                    <li>Salt & Sugar</li>
-                    <li>Rice</li>
-                    <li>Dal or Lentil</li>
-                    <li>Ready Mix</li>
-                    <li>Oil</li>
-                    <li>Ghee</li>
-                </ul>
-            </li>
-        </ul>
-    </div>
+    <section class="layout-sidebar deviation-list w-[100%]">
+        <div id="accordion-collapse" data-accordion="collapse">
+            <h2 id="accordion-collapse-heading-1">
+                <button type="button"
+                    class="flex items-center justify-between w-full p-2 font-medium rtl:text-right text-gray-500 border border-b-0 border-gray-200 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3"
+                    data-accordion-target="#accordion-collapse-body-1" aria-expanded="false"
+                    aria-controls="accordion-collapse-body-1">
+                    
+                    <span>Fruits & Vegetables</span>
+                    <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M9 5 5 1 1 5" />
+                    </svg>
+                </button>
+            </h2>
+            <div id="accordion-collapse-body-1" class="hidden" aria-labelledby="accordion-collapse-heading-1">
+                <div class="p-2 border border-b-0 border-gray-200 dark:border-gray-700 dark:bg-gray-900">
+
+                    <ul class="ms-5">
+                        <li>Fresh Vegetables</li>
+                        <li>Fresh Fruits</li>
+                    </ul>
+                </div>
+            </div>
+            <h2 id="accordion-collapse-heading-2">
+                <button type="button"
+                    class="flex items-center justify-between w-full p-2 font-medium rtl:text-right text-gray-500 border border-b-0 border-gray-200 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3"
+                    data-accordion-target="#accordion-collapse-body-2" aria-expanded="false"
+                    aria-controls="accordion-collapse-body-2">
+                    <span>Meat & Fish</span>
+                    <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M9 5 5 1 1 5" />
+                    </svg>
+                </button>
+            </h2>
+            <div id="accordion-collapse-body-2" class="hidden" aria-labelledby="accordion-collapse-heading-2">
+                <div class="p-2 border border-b-0 border-gray-200 dark:border-gray-700">
+
+                    <ul class="ms-5">
+                        <li>Chicken & Poultry</li>
+                        <li>Premium Perishables</li>
+                        <li>Frozen Fish</li>
+                        <li>Meat</li>
+                        <li>Dried Fish</li>
+                    </ul>
+                </div>
+            </div>
+            <h2 id="accordion-collapse-heading-3">
+                <button type="button"
+                    class="flex items-center justify-between w-full p-2 font-medium rtl:text-right text-gray-500 border border-gray-200 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3"
+                    data-accordion-target="#accordion-collapse-body-3" aria-expanded="false"
+                    aria-controls="accordion-collapse-body-3">
+                    <span>Cooking</span>
+                    <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M9 5 5 1 1 5" />
+                    </svg>
+                </button>
+            </h2>
+            <div id="accordion-collapse-body-3" class="hidden" aria-labelledby="accordion-collapse-heading-3">
+                <div class="p-2 border border-t-0 border-gray-200 dark:border-gray-700">
+
+                    <ul class="ms-5">
+                        <li>Spices</li>
+                        <li>Salt & Sugar</li>
+                        <li>Rice</li>
+                        <li>Dal or Lentil</li>
+                        <li>Ready Mix</li>
+                        <li>Oil</li>
+                        <li>Ghee</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+
+
     </section>
 </template>
 
