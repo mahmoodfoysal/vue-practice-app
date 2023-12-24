87950915ce0cb56c8600747318c8654d121d786a
mahmoodfoysal <foysalcse033@gmail.com>
2023-12-24 15:10:53 +0600

    modal remove

--- src/components/Home/DashBoardHome/DashBoardHome.vue
+++ src/components/Home/DashBoardHome/DashBoardHome.vue
@@ -56,9 +56,7 @@ const { divisionDistrictInfo } = toRefs(props)
                 <button type="submit"
                     class="mx-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
             </form>
-
         </section>
-
         <h1 class="text-3xl text-center" v-if="divisionDistrictInfo === null">Please select Your District</h1>
         <section v-else class="main-container bg-[#F3F4F6] rounded-lg w-[100%] h-[100%] overflow-hidden">
             <div>
@@ -72,13 +70,10 @@ const { divisionDistrictInfo } = toRefs(props)
             data-modal-target="static-modal" data-modal-toggle="static-modal" 
             class="text-white bg-[#7C3AED] rounded-md px-[15px] mx-5 text-xs">Add New</button> -->
 
-
                 </div>
             </div>
 
-
             <!-- table start -->
-
 
             <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
                 <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
@@ -105,7 +100,6 @@ const { divisionDistrictInfo } = toRefs(props)
                             <th scope="col" class="px-6 py-3">
                                 Female
                             </th>
-
                         </tr>
                     </thead>
                     <tbody v-for="districtInfo in divisionDistrictInfo">
@@ -132,47 +126,16 @@ const { divisionDistrictInfo } = toRefs(props)
                             <td class="px-6 py-2">
                                 dummy
                             </td>
-
-
                         </tr>
-
-
-
                     </tbody>
                 </table>
             </div>
 
             <!-- table end -->
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
             <!-- value, user, ticket  -->
             <!-- dashboard features start -->
+
             <!-- <section class="flex gap-x-5 px-5 mt-5">
             <div class="flex items-center justify-between w-1/4 rounded-lg px-4 box-border bg-[#FFFFFF] z-10 h-16">
                 <div>
@@ -214,59 +177,6 @@ const { divisionDistrictInfo } = toRefs(props)
             <!-- dashboard features end  -->
 
         </section>
-
-        <!-- modal start -->
-
-
-        <!-- Modal toggle -->
-        <!-- Main modal -->
-        <div id="static-modal" data-modal-backdrop="static" tabindex="-1" aria-hidden="true"
-            class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
-            <div class="relative p-4 w-full max-w-2xl max-h-full">
-                <!-- Modal content -->
-                <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                    <!-- Modal header -->
-                    <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
-                        <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
-                            Static modal
-                        </h3>
-                        <button type="button"
-                            class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-                            data-modal-hide="static-modal">
-                            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                                viewBox="0 0 14 14">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                    d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
-                            </svg>
-                            <span class="sr-only">Close modal</span>
-                        </button>
-                    </div>
-                    <!-- Modal body -->
-                    <div class="p-4 md:p-5 space-y-4">
-                        <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                            With less than a month to go before the European Union enacts new consumer privacy laws for its
-                            citizens, companies around the world are updating their terms of service agreements to comply.
-                        </p>
-                        <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                            The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25
-                            and is meant to ensure a common set of data rights in the European Union. It requires
-                            organizations to notify users as soon as possible of high-risk data breaches that could
-                            personally affect them.
-                        </p>
-                    </div>
-                    <!-- Modal footer -->
-                    <div class="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
-                        <button data-modal-hide="static-modal" type="button"
-                            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">I
-                            accept</button>
-                        <button data-modal-hide="static-modal" type="button"
-                            class="ms-3 text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Decline</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <!-- modal end -->
 
     </div>
 </template>
